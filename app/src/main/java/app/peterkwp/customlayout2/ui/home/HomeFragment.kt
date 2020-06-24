b8134023dee1b6378d1f326baad9fbaf539c48f4
parkkw09 <parkkw09@gmail.com>
2020-06-25 00:03:14 +0900

    create CustomDialog

--- app/src/main/java/app/peterkwp/customlayout2/ui/home/HomeFragment.kt
+++ app/src/main/java/app/peterkwp/customlayout2/ui/home/HomeFragment.kt
@@ -1,9 +1,11 @@
 package app.peterkwp.customlayout2.ui.home
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import androidx.appcompat.widget.Toolbar
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
@@ -12,6 +14,7 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import app.peterkwp.customlayout2.R
 import app.peterkwp.customlayout2.adapter.DefaultAdapter
+import app.peterkwp.customlayout2.dialog.CustomDialog
 
 class HomeFragment : Fragment() {
 
@@ -28,9 +31,24 @@ class HomeFragment : Fragment() {
         val root = inflater.inflate(R.layout.fragment_home, container, false)
         val list: RecyclerView = root.findViewById(R.id.recycler)
         val fragmentToolbar: Toolbar = root.findViewById(R.id.toolbar)
+        val image: ImageView = root.findViewById(R.id.image)
         homeViewModel.text.observe(this, Observer {
             fragmentToolbar.title = it
         })
+
+        image.setOnClickListener {
+            context?.run {
+                CustomDialog.Builder(this)
+                    .setButtonTitle("확인")
+                    .setView(ImageView(this).apply {
+                        setImageResource(R.drawable.iu01)
+                    })
+                    .setListener {
+                        Log.d("HomeFragment", "Custom Dialog")
+                    }
+                    .show()
+            }
+        }
 
         list.apply {
             layoutManager = LinearLayoutManager(context)
